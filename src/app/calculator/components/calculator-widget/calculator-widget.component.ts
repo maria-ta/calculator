b7565ae68bb2a97a075c95041d4b2953e965cf39
maria-ta <marytana58@gmail.com>
2020-04-25 12:46:48 +0200

    Add basic view logic to Calculator Widget

--- src/app/calculator/components/calculator-widget/calculator-widget.component.ts
+++ src/app/calculator/components/calculator-widget/calculator-widget.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+type AddCharFun = () => void;
+
+
 @Component({
   selector: 'app-calculator-widget',
   templateUrl: './calculator-widget.component.html',
@@ -8,10 +11,40 @@ import { Component, OnInit } from '@angular/core';
 export class CalculatorWidgetComponent implements OnInit {
 
   inputString = '';
+  showAllButtons = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleButtons() {
+    this.showAllButtons = !this.showAllButtons;
+  }
+
+  clearInput() {
+    this.inputString = '';
+  }
+
+  inputChange($event) {
+    this.inputString = $event.target.value;
+  }
+
+  addChar(char: string): AddCharFun {
+    return () => {
+      this.inputString += char;
+    };
+  }
+
+  backspace() {
+    this.inputString = this.inputString.substring(0, this.inputString.length - 1);
+  }
+
+  percentage() {
+    const numberStr = Number(this.inputString);
+    if (!isNaN(numberStr)) {
+      this.inputString = `${numberStr / 100.0}`;
+    }
   }
 
 }
